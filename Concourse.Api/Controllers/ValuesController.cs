d34255bb2fb34b3aa410b1f14dff4d36a40a7435
Simon Huang <simon.huang@devopslounge.com>
2019-04-05 11:03:17 +1300

    Update to dotnet 2.1, try fix pipeline error

--- Concourse.Api/Controllers/ValuesController.cs
+++ Concourse.Api/Controllers/ValuesController.cs
@@ -14,23 +14,19 @@ namespace Concourse.Api.Controllers
       public IEnumerable<string> Get() {
          return new string[] { "value1", "value2", "value3", "value4","value5" };
       }
-
       // GET api/values/5
       [HttpGet("{id}")]
       public string Get( int id ) {
          return "value";
       }
-
       // POST api/values
       [HttpPost]
       public void Post( [FromBody]string value ) {
       }
-
       // PUT api/values/5
       [HttpPut("{id}")]
       public void Put( int id, [FromBody]string value ) {
       }
-
       // DELETE api/values/5
       [HttpDelete("{id}")]
       public void Delete( int id ) {
